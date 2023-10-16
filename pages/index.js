8b968ad1e1da6fab97c2bee0e589df69af56bda5
ZhukovAnd <56645245+ZhukovAnd@users.noreply.github.com>
2023-10-16 22:05:30 +0300

    start git

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+
+import path from 'path';
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/Layout';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import { getSortedPostsData } from '../lib/post';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+const postsDirectory = path.join(process.cwd(), 'posts')
+
+  return (
+    <Layout home>
+      {/* Keep the existing code here */}
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+             <Link href={ postsDirectory + "/" + id }>{title}</Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
